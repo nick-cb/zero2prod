379ce02948e5fef65c4c64e85d3f19973e3f3b14
VI Nguyen <nguyenhungvicb@gmail.com>
2023-03-09 11:50:01 +0700

    First intergration test

--- tests/heath_check.rs
+++ tests/heath_check.rs
@@ -1,10 +1,21 @@
-#[tokio::test]
-async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn our app");
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
 
-    // let client = request_value
+    let _ = tokio::spawn(server);
 }
 
-async fn spawn_app() -> std::io::Result<()> {
-    todo!()
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
